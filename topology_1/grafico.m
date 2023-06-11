a35fec5b9b2249e19290a23a2b7476e3c7746243
Marco <marco.massetti@protonmail.com>
2023-06-11 15:53:13 +0200

    ptodo list

--- topology_1/grafico.m
+++ topology_1/grafico.m
@@ -2,23 +2,23 @@ format long e
 
 %% AC (Driver H)
 figure(1)
-fileID = fopen('ibm_topology_1_AC_H_1H_H.txt','r');
+fileID = fopen('./AC_simulations/ibm_topology_1_AC_H_1H_H.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f (%fdB,%f°)', [3, inf]);
 semilogx(spice(1,:), spice(2,:))
 hold on
 
-fileID = fopen('ibm_topology_1_AC_H_5H_H.txt','r');
+fileID = fopen('./AC_simulations/ibm_topology_1_AC_H_5H_H.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f (%fdB,%f°)', [3, inf]);
 semilogx(spice(1,:), spice(2,:))
 
-fileID = fopen('ibm_topology_1_AC_H_1L_H.txt','r');
+fileID = fopen('./AC_simulations/ibm_topology_1_AC_H_1L_H.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f (%fdB,%f°)', [3, inf]);
 semilogx(spice(1,:), spice(2,:))
 
-fileID = fopen('ibm_topology_1_AC_H_5L_H.txt','r');
+fileID = fopen('./AC_simulations/ibm_topology_1_AC_H_5L_H.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f (%fdB,%f°)', [3, inf]);
 semilogx(spice(1,:), spice(2,:))
@@ -30,23 +30,23 @@ title('AC (Driver H)')
 
 %% AC (Driver L)
 figure(2)
-fileID = fopen('ibm_topology_1_AC_L_1H_L.txt','r');
+fileID = fopen('./AC_simulations/ibm_topology_1_AC_L_1H_L.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f (%fdB,%f°)', [3, inf]);
 semilogx(spice(1,:), spice(2,:))
 hold on
 
-fileID = fopen('ibm_topology_1_AC_L_5H_L.txt','r');
+fileID = fopen('./AC_simulations/ibm_topology_1_AC_L_5H_L.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f (%fdB,%f°)', [3, inf]);
 semilogx(spice(1,:), spice(2,:))
 
-fileID = fopen('ibm_topology_1_AC_L_1L_L.txt','r');
+fileID = fopen('./AC_simulations/ibm_topology_1_AC_L_1L_L.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f (%fdB,%f°)', [3, inf]);
 semilogx(spice(1,:), spice(2,:))
 
-fileID = fopen('ibm_topology_1_AC_L_5L_L.txt','r');
+fileID = fopen('./AC_simulations/ibm_topology_1_AC_L_5L_L.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f (%fdB,%f°)', [3, inf]);
 semilogx(spice(1,:), spice(2,:))
@@ -57,23 +57,23 @@ title('AC (Driver L)')
 
 %% Tran (Driver H)
 figure(3)
-fileID = fopen('ibm_topology_1_TRAN_H_1H_H.txt','r');
+fileID = fopen('./TRAN_simulations/ibm_topology_1_TRAN_H_1H_H.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f %f', [2, inf]);
 plot(spice(1,:), spice(2,:))
 hold on
 
-fileID = fopen('ibm_topology_1_TRAN_H_5H_H.txt','r');
+fileID = fopen('./TRAN_simulations/ibm_topology_1_TRAN_H_5H_H.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f %f', [2, inf]);
 plot(spice(1,:), spice(2,:))
 
-fileID = fopen('ibm_topology_1_TRAN_H_1L_H.txt','r');
+fileID = fopen('./TRAN_simulations/ibm_topology_1_TRAN_H_1L_H.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f %f', [2, inf]);
 plot(spice(1,:), spice(2,:))
 
-fileID = fopen('ibm_topology_1_TRAN_H_5L_H.txt','r');
+fileID = fopen('./TRAN_simulations/ibm_topology_1_TRAN_H_5L_H.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f %f', [2, inf]);
 plot(spice(1,:), spice(2,:))
@@ -84,23 +84,23 @@ title('Tran (Driver H)')
 
 %% Tran (Driver L)
 figure(4)
-fileID = fopen('ibm_topology_1_TRAN_L_1H_L.txt','r');
+fileID = fopen('./TRAN_simulations/ibm_topology_1_TRAN_L_1H_L.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f %f', [2, inf]);
 plot(spice(1,:), spice(2,:))
 hold on
 
-fileID = fopen('ibm_topology_1_TRAN_L_5H_L.txt','r');
+fileID = fopen('./TRAN_simulations/ibm_topology_1_TRAN_L_5H_L.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f %f', [2, inf]);
 plot(spice(1,:), spice(2,:))
 
-fileID = fopen('ibm_topology_1_TRAN_L_1L_L.txt','r');
+fileID = fopen('./TRAN_simulations/ibm_topology_1_TRAN_L_1L_L.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f %f', [2, inf]);
 plot(spice(1,:), spice(2,:))
 
-fileID = fopen('ibm_topology_1_TRAN_L_5L_L.txt','r');
+fileID = fopen('./TRAN_simulations/ibm_topology_1_TRAN_L_5L_L.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f %f', [2, inf]);
 plot(spice(1,:), spice(2,:))
@@ -111,23 +111,23 @@ title('Tran (Driver L)')
 
 %% Tran (Long pulse)
 figure(5)
-fileID = fopen('ibm_topology_1_TRAN_H_1H_H_2.txt','r');
+fileID = fopen('./TRAN_simulations/ibm_topology_1_TRAN_H_1H_H_2.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f %f', [2, inf]);
 plot(spice(1,:), spice(2,:))
 hold on
 
-fileID = fopen('ibm_topology_1_TRAN_H_5H_H_2.txt','r');
+fileID = fopen('./TRAN_simulations/ibm_topology_1_TRAN_H_5H_H_2.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f %f', [2, inf]);
 plot(spice(1,:), spice(2,:))
 
-fileID = fopen('ibm_topology_1_TRAN_L_1L_L_2.txt','r');
+fileID = fopen('./TRAN_simulations/ibm_topology_1_TRAN_L_1L_L_2.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f %f', [2, inf]);
 plot(spice(1,:), spice(2,:))
 
-fileID = fopen('ibm_topology_1_TRAN_L_5L_L_2.txt','r');
+fileID = fopen('./TRAN_simulations/ibm_topology_1_TRAN_L_5L_L_2.txt','r');
 fgetl(fileID);
 [spice] = fscanf(fileID, '%f %f', [2, inf]);
 plot(spice(1,:), spice(2,:))
