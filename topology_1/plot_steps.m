17e9f0c3b35001961ef110fe27febac57de0ff6a
Marco <marco.massetti@protonmail.com>
2023-06-11 17:02:22 +0200

    Improved plot steps

--- topology_1/plot_steps.m
+++ topology_1/plot_steps.m
@@ -7,19 +7,19 @@ fileID = fopen('test.txt','r');
 steps = [];
 fgetl(fileID);
 
-
-steps = [steps extractBetween(fgetl(fileID), ": ","  ")];
-[spice] = fscanf(fileID, '%f %f', [2, inf]);
+line = fgetl(fileID);
+n_steps = extractBetween(line, "/", ")");
+n_steps = str2double(n_steps{1});
+steps = [steps extractBetween(line, ": ", "  ")];
+[spice] = fscanf(fileID, '%f (%fdB,%f°)', [3, inf]);
 plot(spice(1,:), spice(2,:))
 hold on
 
-steps = [steps extractBetween(fgetl(fileID), ": ","  ")];
-[spice] = fscanf(fileID, '%f %f', [2, inf]);
-plot(spice(1,:), spice(2,:))
+for i=1:(n_steps-1)
+    steps = [steps extractBetween(fgetl(fileID), ": ","  ")];
+    [spice] = fscanf(fileID, '%f (%fdB,%f°)', [3, inf]);
+    plot(spice(1,:), spice(2,:))
+end
 
-steps = [steps extractBetween(fgetl(fileID), ": ","  ")];
-[spice] = fscanf(fileID, '%f %f', [2, inf]);
-plot(spice(1,:), spice(2,:))
-
-
+grid minor
 legend(steps{:,:})
