544db6271b24c0882620d42493ac6d3bb81f6829
Marco <marco.massetti@protonmail.com>
2023-06-17 18:30:15 +0200

    FFE 5 Gbps

---
+++ topology_2/FFE_5G.m
@@ -0,0 +1,32 @@
+format long e
+
+x = [-9.59143e-007; 0.402296; -0.0212433; 0.0289993; -0.0145152; -0.00167035];
+y = [0; 1; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0];
+
+A = zeros(12,6);
+
+for i=1:6
+    A(i:(i+5),i) = x;
+end
+
+c = A\y
+c_n = c./(sum(abs(c)))
+y_approx = A*c
+y_approx_n = A*c_n
+
+f = linspace(1e3,40e9, 1e3);
+C_f = zeros(1,length(f));
+Tb = 0.1e-9;
+
+% for i=3:9
+%     n = i-5;
+%     C_f = C_f + c(i)*exp(-1i*2*pi*f*Tb*n);
+% end
+% 
+% semilogx(f, 20*log10(abs(C_f)))
+% hold on
+% grid minor
+% title('FIR filter frequency response')
+% xlabel('frequency (Hz)')
+% ylabel('Magnitude (dB)')
+% xlim([1e3, 40e9])
